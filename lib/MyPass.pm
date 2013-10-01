7e5062e2fd60200b03929a70a9909fa5f1905a19
Stephan Grein <stephan@syntaktischer-zucker.de>
2013-10-01 19:50:23 +0200

    Adding MyPass

---
+++ lib/MyPass.pm
@@ -0,0 +1,29 @@
+#
+#===============================================================================
+#
+#         FILE: MyPass.pm
+#
+#  DESCRIPTION: 
+#
+#        FILES: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 10/01/2013 15:53:05
+#     REVISION: ---
+#===============================================================================
+
+package MyPass;
+
+use strict;
+use warnings;
+
+use Exporter;
+our @ISA= qw( Exporter );
+our @EXPORT = qw( %mail );
+#
+our %mail = (
+        user => 'grein@informatik.uni-frankfurt.de',
+        password => '');
